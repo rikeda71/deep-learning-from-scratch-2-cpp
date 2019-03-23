c2afd49b324ba5abb18e9b639896b6399868f359
Ryuya Ikeda <rikeda71@gmail.com>
2019-03-24 02:10:04 +0900

    Fix: code format

--- ch0/plot_sample.cpp
+++ ch0/plot_sample.cpp
@@ -1,17 +1,19 @@
-#include<iostream>
-#include"matplotlibcpp.h"
+#include <iostream>
+#include "matplotlibcpp.h"
 using namespace std;
 
 namespace plt = matplotlibcpp;
 
-int main(){
-  cout<<"matplotlib-cpp sample start"<<endl;
+int main()
+{
+  cout << "matplotlib-cpp sample start" << endl;
 
   int n = 5000;
   vector<double> x(n), y(n);
-  for(int i=0; i<n; ++i) {
+  for (int i = 0; i < n; ++i)
+  {
     x.at(i) = i;
-    y.at(i) = sin(2*M_PI*i/240.0);
+    y.at(i) = sin(2 * M_PI * i / 240.0);
   }
 
   plt::plot(x, y, "--r");
